88544273dfde793b9f1b9cefe76d6edf74fc0236
Weiqi Liu <bblwq@users.noreply.github.com>
2020-03-20 11:41:25 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-
+$(document).ready(function() {
+	$("#home-link").click(function() {
+		$('a').removeClass('active');
+		$(this).addClass('active');
+		$(".tab").hide();
+		$('#tab-home').show();
+	});
+	$("#data-link").click(function() {
+		$('a').removeClass('active');
+		$(this).addClass('active');
+		$(".tab").hide();
+		$('#tab-data').show();
+	});
+	$("#news-link").click(function() {
+		$('a').removeClass('active');
+		$(this).addClass('active');
+		$(".tab").hide();
+		$('#tab-news').show();
+	});
+	$("#contact-link").click(function() {
+		$('a').removeClass('active');
+		$(this).addClass('active');
+		$(".tab").hide();
+		$('#tab-contact').show();
+	});
+});
