2de4c6c93dbaafdb25cf4b15c50c116200eda848
dazhuanger <49572504+dazhuanger@users.noreply.github.com>
2020-03-20 21:10:06 -0400

    a tags to buttons

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 console.log("main.js loaded");
 $(document).ready(function() {
 	$("#home-link").click(function() {
-		$('a').removeClass('active');
+		$('button').removeClass('active');
 		$(this).addClass('active');
 		$(".tab").hide();
 		$('#tab-home').show();
 	});
 	$("#data-link").click(function() {
-		$('a').removeClass('active');
+		$('button').removeClass('active');
 		$(this).addClass('active');
 		$(".tab").hide();
 		$('#tab-data').show();
 	});
 	$("#news-link").click(function() {
-		$('a').removeClass('active');
+		$('button').removeClass('active');
 		$(this).addClass('active');
 		$(".tab").hide();
 		$('#tab-news').show();
 	});
 	$("#supplies-link").click(function() {
-		$('a').removeClass('active');
+		$('button').removeClass('active');
 		$(this).addClass('active');
 		$(".tab").hide();
 		$('#tab-supplies').show();
