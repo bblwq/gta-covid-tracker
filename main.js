7055ff13675cfef9c16ef972034f85a1eac47a56
dazhuanger <49572504+dazhuanger@users.noreply.github.com>
2020-03-23 17:30:42 -0400

    modified button classes

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 console.log("main.js loaded");
 $(document).ready(function() {
 	$("#home-link").click(function() {
-		$('button').removeClass('active');
+		$(".tab-btn").removeClass('active');
 		$(this).addClass('active');
 		$(".tab").hide();
 		$('#tab-home').show();
 	});
 	$("#data-link").click(function() {
-		$('button').removeClass('active');
+		$(".tab-btn").removeClass('active');
 		$(this).addClass('active');
 		$(".tab").hide();
 		$('#tab-data').show();
@@ -19,13 +19,13 @@ $(document).ready(function() {
         }
     });
 	$("#news-link").click(function() {
-		$('button').removeClass('active');
+		$(".tab-btn").removeClass('active');
 		$(this).addClass('active');
 		$(".tab").hide();
 		$('#tab-news').show();
 	});
 	$("#supplies-link").click(function() {
-		$('button').removeClass('active');
+		$(".tab-btn").removeClass('active');
 		$(this).addClass('active');
 		$(".tab").hide();
 		$('#tab-supplies').show();
